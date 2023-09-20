091ffdfdaa5d3ea6a055147424d5a0f989e81ac9
David Cuen <dccuen@gmail.com>
2023-09-20 16:22:23 -0700

    added ability to select pen size and started working on erase/color funcitons

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function createGrid(size) {
     let height, width;
 
     if (size === 'small') {
-        height = 16;
-        width = 22;
-    } else if (size === 'large') {
         height = 64;
         width = 88;
+    } else if (size === 'large') {
+        height = 16;
+        width = 22;
     } else {
         height = 32;
         width = 44;
@@ -25,12 +25,60 @@ function createGrid(size) {
         const gridContainer = document.getElementById('grid-container');
         gridContainer.appendChild(gridBox);
     }
-}
 
-createGrid('small');
-const gridBox = document.getElementsByClassName('grid-box');
-Array.from(gridBox).forEach(element => {
-    element.addEventListener('mouseover', () => {
-        element.style.backgroundColor = 'gray';
+    const gridBoxes = document.getElementsByClassName('grid-box');
+    Array.from(gridBoxes).forEach(box => {
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'gray';
     })
 });
+}
+
+function clearGrid() {
+    const gridContainer = document.getElementById('grid-container');
+
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.childNodes[0]);
+    }
+}
+
+function resetGrid(size) {
+    clearGrid();
+    createGrid(size);
+}
+
+let currentSize = 'small';
+createGrid(currentSize);
+
+const sizeSelect = document.querySelectorAll('.box-size');
+sizeSelect.forEach(element => {
+    element.addEventListener('click', () => {
+        switch (element.id) {
+            case 'small-size-square':
+                currentSize = 'small';
+                break;
+
+            case 'medium-size-square':
+                currentSize = 'medium';
+                break;
+
+            case 'large-size-square':
+                currentSize = 'large';
+                break;
+        
+            default:
+                currentSize = 'medium';
+                break;
+        }
+
+        resetGrid(currentSize);
+
+        // remove border around all squares
+        sizeSelect.forEach(size => {
+            size.classList.remove('selected-size')
+        })
+
+        // selected square gets a border
+        element.classList.add('selected-size');
+    })
+})
