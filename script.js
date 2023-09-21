9d0a34716cc49ea5031248b425284041643c3648
David Cuen <dccuen@gmail.com>
2023-09-21 11:30:59 -0700

    completed controls and changed some minor css - project complete

--- script.js
+++ script.js
@@ -25,13 +25,6 @@ function createGrid(size) {
         const gridContainer = document.getElementById('grid-container');
         gridContainer.appendChild(gridBox);
     }
-
-    const gridBoxes = document.getElementsByClassName('grid-box');
-    Array.from(gridBoxes).forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'gray';
-    })
-});
 }
 
 function clearGrid() {
@@ -47,9 +40,81 @@ function resetGrid(size) {
     createGrid(size);
 }
 
-let currentSize = 'small';
-createGrid(currentSize);
+function classicColoring() {
+    const gridBoxes = document.getElementsByClassName('grid-box');
+    Array.from(gridBoxes).forEach(box => {
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'gray';
+        })
+    });
+}
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function modernColoring() {
+    const gridBoxes = document.getElementsByClassName('grid-box');
+    Array.from(gridBoxes).forEach(box => {
+    box.addEventListener('mouseover', () => {
+        const randomColor = getRandomColor();
+        box.style.backgroundColor = randomColor;
+        })
+    });
+}
+
+function setColoring(mode) {
+    if (mode === 'modern') {
+        modernColoring();
+    } else {
+        classicColoring();
+    }
+}
+
+
+// start with default normal sized grid and classic coloring
+let currentSize = 'medium',
+    coloringMode = 'classic';
+createGrid(currentSize);
+setColoring(coloringMode);
+
+// change coloring
+const colorSelect = document.querySelectorAll('.box-color');
+colorSelect.forEach(element => {
+    element.addEventListener('click', () => {
+        if (element.id === 'modern') {
+            coloringMode = 'modern';
+        } else {
+            coloringMode = 'classic';
+        }
+
+        resetGrid(currentSize);
+        setColoring(coloringMode);
+
+        // remove border around all squares
+        colorSelect.forEach(color => {
+            color.classList.remove('selected-color')
+        })
+
+        // selected square gets a border
+        element.classList.add('selected-color');
+    })
+})
+
+// erase grid
+const erase = document.querySelector('#erase');
+console.log(erase);
+erase.addEventListener('click', () => {
+    resetGrid(currentSize);
+    setColoring(coloringMode);
+})
+
+// change pencil size
 const sizeSelect = document.querySelectorAll('.box-size');
 sizeSelect.forEach(element => {
     element.addEventListener('click', () => {
@@ -72,6 +137,7 @@ sizeSelect.forEach(element => {
         }
 
         resetGrid(currentSize);
+        setColoring(coloringMode);
 
         // remove border around all squares
         sizeSelect.forEach(size => {
